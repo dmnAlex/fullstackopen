174c2d61d8d49ab8a330450b38636c153620f0dd
Alex <logical@ngs.ru>
2021-01-14 02:21:33 +0700

    Completed part 4

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,8 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const loginRouter = require('./controllers/login')
+const userRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -18,9 +20,13 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     logger.error('error connecting to MongoDB:', error.message)
   })
 
+app.use(middleware.tokenExtractor)
+
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter)
+app.use('/api/users', userRouter)
 app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
