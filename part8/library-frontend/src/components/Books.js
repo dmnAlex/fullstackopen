ac9adfb5af24b92447220a354818b8b1f8bb9265
Alex <logical@ngs.ru>
2021-03-04 18:41:54 +0700

    completed 8.17

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -4,6 +4,7 @@ import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
+  console.log('res = ', result)
   if (!props.show) {
     return null
   }
@@ -30,7 +31,7 @@ const Books = (props) => {
           {books.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
-              <td>{a.author}</td>
+              <td>{a.author.name}</td>
               <td>{a.published}</td>
             </tr>
           )}
