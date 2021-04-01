e2c1ec3b5d0b499289d5066a657bdb54d40608dd
Alex <logical@ngs.ru>
2021-04-01 22:54:26 +0700

    add entry modal for healthcheck type

--- part9/patientor-frontend/src/PatientPage/index.tsx
+++ part9/patientor-frontend/src/PatientPage/index.tsx
@@ -1,13 +1,16 @@
 import axios from 'axios';
 import React from 'react';
 import { useParams } from 'react-router';
-import { Container, Icon } from 'semantic-ui-react';
+import { Button, Container, Icon } from 'semantic-ui-react';
+import AddEntryModal from '../AddEntryModal';
 import { apiBaseUrl } from '../constants';
 import { updatePatient, useStateValue } from '../state';
-import { Patient, Entry, GenderIcon } from '../types';
+import { Patient, Entry, GenderIcon, HealthCheckEntry } from '../types';
 import EntryDetails from './EntryDetails';
 
 const PatientPage = () => {
+  const [modalOpen, setModalOpen] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | undefined>();
   const [{ patients }, dispatch] = useStateValue();
   const { id } = useParams<{ id: string }>();
   const fetchPatient = async () => {
@@ -19,6 +22,31 @@ const PatientPage = () => {
     } catch (e) {
       console.error(e);
     }
+  };
+  const submitNewEntry = async (values: Omit<HealthCheckEntry, 'id'>) => {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const { data: entryFromApi } = await axios.post<Entry>(
+        `${apiBaseUrl}/patients/${id}/entries`,
+        values
+      );
+      const updatedPatient = {
+        ...patient,
+        entries: patient.entries.concat(entryFromApi)
+      };
+      dispatch(updatePatient(updatedPatient));
+      closeModal();
+    } catch (e) {
+      console.error(e.response?.data || 'Unknown Error');
+      setError(e.response?.data || 'Unknown error');
+    }
+  };
+
+  const openModal = (): void => setModalOpen(true);
+
+  const closeModal = (): void => {
+    setModalOpen(false);
+    setError(undefined);
   };
 
   const patient: Patient = patients[id];
@@ -39,6 +67,13 @@ const PatientPage = () => {
           <EntryDetails key={entry.id} entry={entry} />
         ))}
       </Container>
+      <AddEntryModal
+        modalOpen={modalOpen}
+        onSubmit={submitNewEntry}
+        onClose={closeModal}
+        error={error}
+      />
+      <Button onClick={() => openModal()}>Add New Entry</Button>
     </div>
   );
 };
